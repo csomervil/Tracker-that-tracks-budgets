3880a58ce4a5b944da8cbe01913cca052453074b
csomervil <carson.somerville@mail.utoronto.ca>
2022-06-28 13:37:38 -0400

    added service worker, and indexedDB for offline functionality

---
+++ public/js/idb.js
@@ -0,0 +1,68 @@
+let db;
+
+const request = indexedDB.open('tracker_budget', 1);
+
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_tracker', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+      uploadTracker();
+    }
+  };
+  
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+
+const transaction = db.transaction(['new_tracker'], 'readwrite');
+
+
+const trackerObjectStore = transaction.objectStore('new_tracker');
+
+
+trackerObjectStore.add(record);
+}
+
+function uploadTracker() {
+    const transaction = db.transaction(['new_tracker'], 'readwrite');
+  
+    const trackerObjectStore = transaction.objectStore('new_tracker');
+  
+    const getAll = trackerObjectStore.getAll();
+  
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['new_tracker'], 'readwrite');
+          const trackerObjectStore = transaction.objectStore('new_tracker');
+          trackerObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadTracker);
